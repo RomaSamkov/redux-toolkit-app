9f073c0e11a529557726d2417503a76d1f2e8f12
RomaSamkov <Romansamkov.rs@gmail.com>
2024-10-22 20:12:44 +0300

    add buttons and AppDispatch

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../state/store";
-import { decrement, increment } from "../state/counter/counterSlice";
+import { AppDispatch, RootState } from "../state/store";
+import {
+  decrement,
+  increment,
+  incrementAsync,
+  incrementByAmount,
+} from "../state/counter/counterSlice";
 
 const Counter = () => {
   const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   return (
     <div>
       <h2>{count}</h2>
       <div className="buttons">
         <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(incrementAsync(10))}>
+          IncrementAsync
+        </button>
+        <button onClick={() => dispatch(incrementByAmount(10))}>
+          IncrementByAmount
+        </button>
         <button onClick={() => dispatch(decrement())}>Decrement</button>
       </div>
     </div>
