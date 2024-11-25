28ba310a82490801fdcdeeac80c800cc392c5449
RomaSamkov <Romansamkov.rs@gmail.com>
2024-11-25 14:31:26 +0200

    Update Images.tsx

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,8 +1,16 @@
+import redux from "../assets/redux.svg";
+import typescript from "../assets/typescript.svg";
+
 const Images = () => {
   return (
     <div className="images">
-      <img src="../assets/redux.svg" alt="redux" height={100} />
-      <img src="../assets/typescript.svg" alt="typescript" height={100} />
+      <img src={redux} alt="redux" height={100} />
+      <img
+        src={typescript}
+        alt="typescript"
+        height={100}
+        style={{ borderRadius: 12 }}
+      />
     </div>
   );
 };
