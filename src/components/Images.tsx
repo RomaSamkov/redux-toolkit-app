eb5f247f61c9b3adca310f8732466c93136f5e1c
RomaSamkov <Romansamkov.rs@gmail.com>
2024-11-25 13:50:43 +0200

    Create Images.tsx

---
+++ src/components/Images.tsx
@@ -0,0 +1,10 @@
+const Images = () => {
+  return (
+    <div className="images">
+      <img src="../assets/redux.svg" alt="redux" height={100} />
+      <img src="../assets/typescript.svg" alt="rtkquery" height={100} />
+    </div>
+  );
+};
+
+export default Images;
