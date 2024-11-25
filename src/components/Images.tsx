90ea384a331b4a04e0a1ed11fcf29024855e940b
RomaSamkov <Romansamkov.rs@gmail.com>
2024-11-25 13:58:25 +0200

    Update Images.tsx

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -2,7 +2,7 @@ const Images = () => {
   return (
     <div className="images">
       <img src="../assets/redux.svg" alt="redux" height={100} />
-      <img src="../assets/typescript.svg" alt="rtkquery" height={100} />
+      <img src="../assets/typescript.svg" alt="typescript" height={100} />
     </div>
   );
 };
