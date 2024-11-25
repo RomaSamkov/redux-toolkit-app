f4fecbff0b9a5778387b4a9f1d06b3d49604ac76
RomaSamkov <Romansamkov.rs@gmail.com>
2024-11-25 13:50:57 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Images from "./components/Images";
 
 function App() {
   return (
     <div>
+      <Images />
       <Counter />
     </div>
   );
